2d5b9bd3d285a5afa78887436b1c79871afed511
Malek MAJOULI <mmajouli.prestataire@oui.sncf>
2020-04-02 14:22:44 +0200

    Udpdate our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
-import { Route, Switch, Link } from 'react-router-dom';
-
-const HatsList = (props) => (
-  <div>
-    <h1>HATS PAGE</h1>
-    <Link to={`${props.match.url}/13`} >TO HAT 13</Link>
-    <Link to={`${props.match.url}/17`} >TO HAT 17</Link>
-    <Link to={`${props.match.url}/21`} >TO HAT 21</Link>
-  </div>
-)
-
-const HatDetail = (props) => (
-
-  <div>
-    {console.log(props)}
-    <h1>HATS DETAIL PAGE: {props.match.params.hatsId} </h1>
-  </div>
-)
-
-
+import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsList} />
-        <Route exact path='/hats/:hatsId' component={HatDetail} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
-import { Link } from 'react-router-dom'
 
-const HomePage = (props) => (
+const HomePage = () => (
 
     <div className='homepage'>
-        <button onClick={() => props.history.push('/hats')}>Hats (history prop)</button>
-        <Link to='/hats'>Hats (Simple Link)</Link>
         <Directory />
     </div>
 )
