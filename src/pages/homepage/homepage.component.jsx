da3a8c9fe4aa1f8a9023962068e9d8443918d936
Malek MAJOULI <mmajouli.prestataire@oui.sncf>
2020-04-02 12:17:59 +0200

    Update menu-items to navigate withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
+import { Link } from 'react-router-dom'
 
-const HomePage = () => (
+const HomePage = (props) => (
 
     <div className='homepage'>
+        <button onClick={() => props.history.push('/hats')}>Hats (history prop)</button>
+        <Link to='/hats'>Hats (Simple Link)</Link>
         <Directory />
     </div>
 )
