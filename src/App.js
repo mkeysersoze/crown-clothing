17901fdcaef12a8f015639de85a9152df3b3f01e
Malek MAJOULI <mmajouli.prestataire@oui.sncf>
2020-04-03 11:34:15 +0200

    Fix body layout and other WIP

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import './App.css'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/sigIn' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
