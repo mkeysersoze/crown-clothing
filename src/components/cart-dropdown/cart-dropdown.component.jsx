e27295785bbae6a31cbfbc9a7dfc7942556505e6
Malek MAJOULI <mmajouli.prestataire@oui.sncf>
2020-04-16 11:40:19 +0200

    Update the app with reselect library, replace cart selectors with memorized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,6 +5,7 @@ import './cart-dropdown.styles.scss'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
 import { connect } from 'react-redux'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 const CartDropDown = ({ cartItems }) => (
     <div className='cart-dropdown'>
@@ -17,8 +18,16 @@ const CartDropDown = ({ cartItems }) => (
     </div>
 )
 
-const mapStateToProps = (state) => (
-    { cartItems: state.cart.cartItems }
-)
+const mapStateToProps = (state) => {
+    console.log('cart-dropdown component is being called')
+    return { cartItems: selectCartItems(state) }
+}
+
+// We could also do this
+/*const mapStateToProps = (state) => {
+    console.log('cart-dropdown component is being called')
+    return { cartItems: state.cart.cartItems }
+}
+*/
 
 export default connect(mapStateToProps)(CartDropDown)
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,11 +6,12 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { connect } from 'react-redux'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -19,4 +20,24 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = state => {
+    console.log('I am being called')
+    return {
+        itemCount: selectCartItemsCount(state)
+    }
+}
+
+/* We could keep it like this : 
+
+const mapStateToProps = ({ cart: { cartItems } }) => {
+    console.log('I am being called')
+    return {
+        itemCount: cartItems.reduce(
+            (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+            0
+        )
+    }
+}
+*/
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
