cf0265a9a00aac498e7d6225644bc3f88cf9e529
mkeysersoze <anonymouss.surf@gmail.com>
2020-04-22 14:23:09 +0200

    Add nested routes, fix collection item width, add new collection component, link urls to each collection and add selectCollection selectors

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
+          <Route path='/shop' component={ShopPage} />
           <Route exact path='/signIn'
             render={() =>
               this.props.currentUser ? (
--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -5,13 +5,14 @@ import { selectShopCollections } from '../../redux/shop/shop.selectors'
 import { createStructuredSelector } from 'reselect'
 import { connect } from 'react-redux'
 
-const CollectionsOverview = ({ collections }) => (
+const CollectionsOverview = ({ collections }) => {
+    return (
     <div className='collections-overview'>
         {collections.map(({ id, ...otherCollectionProps }) => (
             <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
     </div>
-)
+)}
 
 const mapStateToProps = createStructuredSelector({
     collections: selectShopCollections
